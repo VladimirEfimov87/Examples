49fd8417cbf4d0ca4133247da327b98a5cb4eef8
Vladimir <rtk2171@gmail.com>
2023-01-06 02:29:01 +0500

    add Example004_difRandom

--- Example004_dif/Program.cs
+++ Example004_dif/Program.cs
@@ -1,3 +1,5 @@
-﻿double num1 = 13;
-double num2 = 5;
+﻿double num1 = new Random().Next(1,10);
+Console.WriteLine(num1);
+double num2 = new Random().Next(1,10);
+Console.WriteLine(num2);
 Console.WriteLine(num1 / num2);
