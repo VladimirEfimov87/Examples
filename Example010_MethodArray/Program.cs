de7d857eb254cb96d7209cdf57a398bbcf8f4e97
Vladimir <rtk2171@gmail.com>
2023-01-11 17:18:07 +0500

    Example010_MethodArray

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 1, 12, 31, 8, 65, 14, 99, 57 };
+
+int n = array.Length;
+int find = 4;
+
+int index = 0;
+
+while (index < n) ;
+{
+
+  if (array[index] == find)
+  {
+    Console.WriteLine(index);
+   }
+  index++;
+}
